9598bbefeb341b07c3e7487bf27202f8fd737b41
Sabbir Alam <dev.sabbir.alam@gmail.com>
2025-12-04 21:04:29 +0600

    Day 4 solution

---
+++ puzzle-4-2.py
@@ -0,0 +1,42 @@
+def findNeighbours(i, j, X, Y):
+    indexes = []
+    dirX = [0, 1, -1, 0, 1, -1, -1, 1]
+    dirY = [1, 0, 0, -1, 1, 1, -1, -1]
+    for d in range(8):
+        newX = i + dirX[d]
+        newY = j + dirY[d]
+        if(newX >= 0 and newY >= 0 and newX < X and newY < Y):
+            indexes.append((newX, newY))
+    return indexes
+def main():
+    filepath = "input.txt"
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+        x = len(lines)
+        y = len(lines[0]) - 1
+        
+        n = 0
+        target = 4
+        newN = -1
+        while(newN != 0):
+            newN = 0
+            
+            for i in range(x):
+                for j in range(y):
+                    indexes = findNeighbours(i, j, x, y)
+                    
+                    total = 0
+                    for (xx, yy) in indexes:
+                        
+                        if(lines[xx][yy] == '@'):
+                            total = total + 1
+                            
+                    if(total < target and lines [i][j] == '@'):
+                        lines[i] = lines[i][:j] + '.' + lines[i][j + 1:]
+                        newN = newN + 1
+                        total = 0
+            n = n + newN
+    print(n)
+            
+main()
+
